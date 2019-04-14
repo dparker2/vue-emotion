3b3a9fb4358f121d1e12ac1e62156f401e444d75
David Parker <crazdave@gmail.com>
2019-04-14 15:58:49 -0700

    v0.0.2

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+// rollup.config.js (building more than one bundle)
+
+export default [{
+  input: 'lib/loader/loader.js',
+  output: {
+    file: 'loader/index.js',
+    format: 'cjs'
+  }
+}, {
+  input: 'lib/plugin/plugin.js',
+  output: {
+    file: 'plugin/index.js',
+    format: 'cjs'
+  }
+}];
