ae321f93dc788ca0fa030364cf172e3cb70feb23
David Parker <crazdave@gmail.com>
2019-04-17 21:40:02 -0700

    publish styled v0.0.2

---
+++ publish.sh
@@ -0,0 +1,21 @@
+if [[ $# < 2 ]]; then
+  echo "
+    Usage: publish [version] [package]
+        version = part to increment (patch|minor|major)
+        package = name of package"
+  exit
+fi
+
+if [[ $1 != 'patch' ]] && [[ $1 != 'minor' ]] && [[ $1 != 'major' ]]; then
+  echo "version must be patch|minor|major"
+  exit
+elif [[ ! -f ./dist/$2/package.json ]]; then
+  echo "./dist/$2/package.json does not exist"
+  exit
+fi
+
+cd ./dist/$2
+VERSION=$(npm version $1)
+cd ../..
+
+git commit -am "publish $2 $VERSION"
