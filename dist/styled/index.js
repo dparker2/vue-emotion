531dc810ba1f42e1d95322e56d91403f7cc495e2
David Parker <crazdave@gmail.com>
2019-04-19 17:01:24 -0700

    add tests with coverage, withcomponent, merge styles properly

--- dist/styled/index.js
+++ dist/styled/index.js
@@ -1,12 +1,19 @@
 'use strict';
 
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var htmlTags = _interopDefault(require('html-tags'));
 var emotion = require('emotion');
 
-const makeMixin = (templateArgs) => ({
+const makeMixin = () => ({
     mounted() {
-        const emotionClass = () => emotion.css.apply(
-            { mergedProps: { ...this.$attrs, ...this.$props } },
-            templateArgs
+        const emotionClass = () => emotion.cx(
+            ...this.$options.$_interpolations.map(templateArgs =>
+                emotion.css.apply(
+                    { mergedProps: { ...this.$attrs, ...this.$props } },
+                    templateArgs
+                )
+            )
         );
 
         const classChanged = (newClass, oldClass) => {
@@ -22,18 +29,61 @@ const makeMixin = (templateArgs) => ({
     }
 });
 
-const styled = function(component) {
-    return function(...templateArgs) {
-        const styledMixin = makeMixin(templateArgs);
-        const mixins = component.mixins;
-        const withStyledMixin = {
-            ...component,
-            mixins: mixins
-                ? [...mixins, styledMixin]
-                : [styledMixin]
-        };
-        return withStyledMixin;
+const makeComponent = (tag) => ({
+    render: function (createElement) {
+        return createElement(
+            tag,
+            this.$attrs,
+            this.$slots.default
+        )
+    }
+});
+
+const withComponent = function(tagOrComp) {
+    const newStyled = styled.hasOwnProperty(tagOrComp)
+        ? styled[tagOrComp]()
+        : styled(tagOrComp)();
+    newStyled.$_interpolations = [
+        ...this.$_interpolations
+    ];
+    return newStyled;
+};
+
+const newStyled = (component) => (...templateArgs) => {
+    const styledMixin = makeMixin();
+    const mixins = component.mixins;
+    const withStyledMixin = {
+        ...component,
+        mixins: mixins
+            ? [...mixins, styledMixin]
+            : [styledMixin],
+        withComponent,
+        $_interpolations: [templateArgs]
+    };
+    return withStyledMixin;
+};
+
+const mergeStyled = (component) => (...templateArgs) => {
+    const $_interpolations = [
+        ...component.$_interpolations,
+        templateArgs
+    ];
+    const withMoreStyles = {
+        ...component,
+        $_interpolations
+    };
+    return withMoreStyles;
+};
+
+const styled = (component) => {
+    if (component.$_interpolations) {
+        return mergeStyled(component)
+    } else {
+        return newStyled(component)
     }
 };
+
+const styledHTML = (tag) => styled(makeComponent(tag));
+htmlTags.forEach(tag => (styled[tag] = styledHTML(tag)));
 
 module.exports = styled;
---
+++ lib/__tests__/Base.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        Base Test Component
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['color'],
+}
+</script>
