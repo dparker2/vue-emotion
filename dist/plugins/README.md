cd080829384f5a25388ed17b391c821f75ddeda8
David Parker <crazdave@gmail.com>
2019-05-04 01:33:28 -0700

    fix dist things

---
+++ dist/plugins/README.md
@@ -0,0 +1,1 @@
+See: https://github.com/ParkerD559/vue-emotion#styled
--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,4 @@
 export default [{
-  input: 'lib/emotion/index.js',
-  output: {
-    file: 'dist/emotion/index.js',
-    format: 'cjs'
-  }
-}, {
   input: 'lib/plugins/index.js',
   output: {
     file: 'dist/plugins/index.js',
